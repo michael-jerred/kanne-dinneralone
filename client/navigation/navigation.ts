102e1ec1ebb39fe72ace5e22480d2d402f41b0ef
Michael Jerred <michaeljerred@googlemail.com>
2015-01-15 20:17:04 +0000

    removing files no longer needed

--- client/navigation/navigation.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../../typings/angularjs/angular.d.ts" />
-/// <reference path="../../typings/angularjs/angular-route.d.ts" />
-
-module App.Navigation {
-
-    var module = angular.module('app.navigation', ['ngRoute, ngMaterial']);
-}
